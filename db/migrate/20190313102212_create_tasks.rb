f2d478ffbec365f06314a9e1907285ea37862d33
yasuda <tasssra@gmail.com>
2019-03-13 19:23:57 +0900

    Scaffold Task title:string memo:text status:integer

---
+++ db/migrate/20190313102212_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :memo
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
