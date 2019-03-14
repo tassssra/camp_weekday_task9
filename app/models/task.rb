092ab9c60522588288dc69d907140c749d7b4827
yasuda <tasssra@gmail.com>
2019-03-14 11:31:30 +0900

    statusの範囲を0~2でしか送信できなくした

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :memo, presence: true, length: 10..30
+  validates :status, presence: true, inclusion: 0..2
+  # TODO: statusを送信する前の時点で0~2しか選択できないようにしたい
 end
