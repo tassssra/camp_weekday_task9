2ea039b35e5d9b1fde0464cde138df38839b6ca1
yasuda <tasssra@gmail.com>
2019-03-14 11:46:03 +0900

    seeds.rbにテストデータを入れてDBに反映

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   validates :memo, presence: true, length: 10..30
   validates :status, presence: true, inclusion: 0..2
   # TODO: statusを送信する前の時点で0~2しか選択できないようにしたい
+  # modelのvalidatesだと↑はできなさそう…
 end
