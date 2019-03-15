86c710e7fd14073f03affd1be277cf8b8912bea5
yasuda <tasssra@gmail.com>
2019-03-15 12:22:59 +0900

    コントローラ修正、不要ファイル削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,36 +17,24 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'タスクを登録しました。' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to @task, notice: 'タスクを登録しました。'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'タスクを更新しました。' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to @task, notice: 'タスクを更新しました。'
+    else
+      render :edit
     end
   end
 
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'タスクを削除しました。' }
-      format.json { head :no_content }
-    end
+    redirect_to tasks_path, notice: 'タスクを削除しました。'
   end
 
   def hide
@@ -54,13 +42,11 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:title, :memo, :status)
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
     end
 end
