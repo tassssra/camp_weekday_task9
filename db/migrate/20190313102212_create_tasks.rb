c5c5975a11ddfc08a2e30d524c4ad73cd9e93eb2
yasuda <tasssra@gmail.com>
2019-03-15 23:32:41 +0900

    マイグレーションを変更

--- db/migrate/20190313102212_create_tasks.rb
+++ db/migrate/20190313102212_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :title, null: false
-      t.text :memo, null: false
+      t.string :title
+      t.text :memo
       t.boolean :is_display, default: true
       t.integer :status
 
